4fa55c83a139579936321d57e2718f7ce8a442b3
Avan Peltier <tex@csh.rit.edu>
2020-12-03 13:18:39 -0600

    Updated Title Comment^

---
+++ Kenny_Button_Code/Kenny_Button_Code.ino
@@ -0,0 +1,24 @@
+/*Kenny Button
+ * Creator: Avan Peltier
+ * 12/3/2020
+ * Plays quotes of the one and only Kenny Kim through
+ * wired speaker
+ */
+void rdm_mp3() {
+  //Randomly chooses mp3 quotes to play 
+
+}
+
+
+void main() {
+  //Plays upon button push
+  
+  //Executes rdm_mp3()
+  
+  //Waits 500 miliseconds
+  
+  //If pressed twice play reset program to rdm_mp3()
+  
+  //Plays rdm_mp3 through speaker at audible frequency 
+
+}

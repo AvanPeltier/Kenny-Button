36a4fb41d8adf72a8743b40bad999d57c7537db8
Avan Peltier <70023651+AvanPeltier@users.noreply.github.com>
2020-12-03 12:50:29 -0600

    Stubbed Out Code

---
+++ Kenny_Button_Code/Kenny_Button_Code.ino
@@ -0,0 +1,23 @@
+void rdm_mp3() {
+  //opens quotes file
+
+  //error check file open
+  
+  //Randomly chooses mp3 quotes to play 
+
+  //closes quotes file
+}
+
+
+void main() {
+  //Plays upon button push
+  
+  //Executes rdm_mp3()
+  
+  //Waits 500 miliseconds
+  
+  //If pressed twice play reset program to rdm_mp3()
+  
+  //Plays rdm_mp3 through speaker at audible frequency 
+
+}
